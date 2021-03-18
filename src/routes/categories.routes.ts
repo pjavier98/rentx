00401b0e178c50984569d896195d9741c68eb7b2
Pedro Javier <pjpc@ic.ufal.br>
2021-03-18 20:54:54 -0300

    feat: create category service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/categories.repository';
+import { CreateCategoryService } from '../services/createCategory.service';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,16 +9,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists' });
-  }
-
-  categoriesRepository.create({
-    name,
-    description,
-  });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
