4b737319c13f7054ba7534263d83213e70093eb9
Pedro Javier <pjpc@ic.ufal.br>
2021-03-19 11:36:11 -0300

    feat: using multer to import file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { findAllCategoriesController } from '../modules/cars/useCases/findAllCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +17,17 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
 categoriesRoutes.get('/', (request: Request, response: Response) => {
   return findAllCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  (request: Request, response: Response) => {
+    const { file } = request;
+
+    console.log(file);
+
+    return response.send();
+  },
+);
 
 export { categoriesRoutes };
